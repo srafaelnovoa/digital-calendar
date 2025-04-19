9c599a30183092fca0c07e2e5d680fc76f3ecd65
srafaelnovoa <rafael.novoa@gmail.com>
2025-04-19 13:35:03 -0400

    Updated to include ID and dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 function App() {
   return (
-    <div className="min-h-screen bg-gray-100 flex flex-col justify-center items-center">
-      <h1 className="text-4xl font-bold text-blue-600">
-        React + Vite + Tailwind
+    <div className="min-h-screen bg-white dark:bg-gray-800 text-gray-900 dark:text-white flex flex-col justify-center items-center">
+      <h1 className="text-4xl font-bold text-blue-600 dark:text-blue-400">
+        Digital Calendar
       </h1>
-      <p className="mt-2 text-gray-600">Digital Calendar</p>
+      <p className="mt-2 text-gray-600 dark:text-gray-300">Start Here</p>
     </div>
   );
 }
